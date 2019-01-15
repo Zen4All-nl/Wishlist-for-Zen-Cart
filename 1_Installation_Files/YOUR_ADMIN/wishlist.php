075a87b0d4e12bcc21fed45fc067d390e192fdc0
Zen4All <info@zen4all.nl>
2019-01-15 15:59:01 +0100

    Rewrite code to get lose over-complicated classes

--- 1_Installation_Files/YOUR_ADMIN/wishlist.php
+++ 1_Installation_Files/YOUR_ADMIN/wishlist.php
@@ -9,9 +9,19 @@ $currencies = new currencies();
 $oWishlist = new un_wishlist();
 
 // Get wishlist
-$id = (isset($_GET['wid']) ? $_GET['wid'] : '');
-if (zen_not_null($id)) {
-  $wishlist = $oWishlist->getWishlist($id);
+$iID = (isset($_GET['wid']) ? (int)$_GET['wid'] : '');
+if ($iID != '') {
+  $sql = "SELECT *
+          FROM " . UN_TABLE_WISHLISTS . " w,
+               " . TABLE_CUSTOMERS . " c
+          WHERE w.id = " . $iID . "
+          AND w.customers_id = c.customers_id";
+
+  $wishlist = $db->Execute($sql);
+  if (!$wishlist) {
+    $messageStack->add('header', 'Error getting wishlist.');
+    return false;
+  }
 } else {
   zen_redirect(UN_FILENAME_WISHLISTS);
   exit;
@@ -28,89 +38,100 @@ if (zen_not_null($action) && zen_not_null($products_id)) {
 }
 
 // Get products in wishlist
-$products_query = $oWishlist->getProductsQuery();
+if (isset($_GET['cPath']) && $_GET['cPath']) {
+  $sql_filter = "AND p2c.categories_id = '" . $_GET['cPath'] . "' ";
+  $sql_from = ", " . TABLE_PRODUCTS_TO_CATEGORIES . " p2c ";
+  $sql_where = " AND p.products_id = p2c.products_id ";
+} else {
+  $sql_filter = '';
+  $sql_from = '';
+  $sql_where = '';
+}
+
+$products_query = "SELECT p2w.products_id,w.customers_id,p2w.created,p2w.quantity,p2w.priority,p2w.comment
+                   FROM " . UN_TABLE_WISHLISTS . " w,
+                        " . UN_TABLE_PRODUCTS_TO_WISHLISTS . " p2w
+                        " . $sql_from . "
+                   WHERE w.id = " . $iID . "
+                   AND w.id = p2w.un_wishlists_id
+                   " . $sql_where . "
+                   " . $sql_filter . "
+                   ORDER BY p2w.products_id";
 $products = $db->Execute($products_query);
 if (!$products) {
   $messageStack->add('header', 'Error getting wishlist products.');
 }
-?><!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+?>
+<!doctype html>
 <html <?php echo HTML_PARAMS; ?>>
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
+    <meta charset="<?php echo CHARSET; ?>">
     <title><?php echo TITLE; ?></title>
-    <link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
-    <link rel="stylesheet" type="text/css" href="includes/cssjsmenuhover.css" media="all" id="hoverJS">
-    <script language="javascript" src="includes/menu.js"></script>
-    <script language="javascript" src="includes/general.js"></script>
-    <script type="text/javascript">
-      <!--
-      function init()
-      {
+    <link rel="stylesheet" href="includes/stylesheet.css">
+    <link rel="stylesheet" href="includes/cssjsmenuhover.css" media="all" id="hoverJS">
+    <script src="includes/menu.js"></script>
+    <script src="includes/general.js"></script>
+    <script>
+      function init() {
           cssjsmenu('navbar');
-          if (document.getElementById)
-          {
+          if (document.getElementById) {
               var kill = document.getElementById('hoverJS');
               kill.disabled = true;
           }
       }
-      // -->
     </script>
   </head>
   <body onload="init()">
     <!-- header //-->
     <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
     <!-- header_eof //-->
+    <div class="container-fluid">
+      <h1><?php echo HEADING_TITLE . TEXT_DELIMITER . $wishlist->fields['name']; ?></h1>
 
-    <h1><?php echo HEADING_TITLE . TEXT_DELIMITER . $wishlist->fields['name']; ?></h1>
+      <ul>
+        <li><?php echo ENTRY_CUSTOMER; ?><a href="<?php echo zen_href_link(FILENAME_CUSTOMERS, 'cID=' . $wishlist->fields['customers_id']); ?>"><?php echo un_get_fullname($wishlist->fields['customers_firstname'], $wishlist->fields['customers_lastname'], $wishlist->fields['customers_email_address']); ?></a></li>
+        <li><?php echo ENTRY_COMMENT . $wishlist->fields['comment']; ?></li>
+        <li><?php echo ENTRY_DEFAULT . $wishlist->fields['default_status']; ?></li>
+        <li><?php echo ENTRY_PUBLIC . $wishlist->fields['public_status']; ?></li>
+      </ul>
 
-    <ul>
-      <li><?php echo ENTRY_CUSTOMER; ?><a href="<?php echo zen_href_link(FILENAME_CUSTOMERS, 'cID=' . $wishlist->fields['customers_id']); ?>"><?php echo un_get_fullname($wishlist->fields['customers_firstname'], $wishlist->fields['customers_lastname'], $wishlist->fields['customers_email_address']); ?></a></li>
-      <li><?php echo ENTRY_COMMENT . $wishlist->fields['comment']; ?></li>
-      <li><?php echo ENTRY_DEFAULT . $wishlist->fields['default_status']; ?></li>
-      <li><?php echo ENTRY_PUBLIC . $wishlist->fields['public_status']; ?></li>
-    </ul>
-
-    <!-- product listing -->
-    <table border="0" width="100%" cellspacing="0" cellpadding="0">
-      <tr class="dataTableHeadingRow">
-
-        <?php echo $oWishlist->getTableHeader(); ?>
-
-      </tr>
-
-      <?php
-      if ($products->RecordCount() > 0) {
-        $rows = 0;
-        while (!$products->EOF) {
-          if ($rows & 1) {
-            $tdclass = 'even';
-          } else {
-            $tdclass = 'odd';
-          }
-          ?>
-
-          <tr>
-
-            <?php echo $oWishlist->getTableRow($tdclass, $products); ?>
-
+      <!-- product listing -->
+      <table class="table table-striped">
+        <thead>
+          <tr class="dataTableHeadingRow">
+            <th><?php echo TABLE_HEADING_PRODUCT; ?></th>
+            <th class="text-right"><?php echo TABLE_HEADING_PRICE; ?></th>
+            <th class="text-center"><?php echo TABLE_HEADING_PRIORITY; ?></th>
+            <th><?php echo TABLE_HEADING_COMMENT; ?></th>
+            <th class="text-right"><?php echo TABLE_HEADING_ACTION; ?></th>
           </tr>
-          <?php $rows++; ?>
-          <?php $products->MoveNext(); ?>
-        <?php } // end while products ?>
+        </thead>
+        <tbody>
+            <?php
+            if ($products->RecordCount() > 0) {
+              foreach ($products as $product) {
+                 ?>
+              <tr>
+                <td><a href="<?php echo zen_href_link(FILENAME_CATEGORIES, '&action=new_product' . '&cPath=' . zen_get_product_path($product['products_id']) . '&pID=' . $product['products_id'] . '&product_type=' . zen_get_products_type($product['products_id'])); ?>"><?php echo zen_get_products_name($product['products_id'], $_SESSION['languages_id']); ?></a></td>
+                <td class="text-right"><?php echo zen_get_products_actual_price($product['products_price']); ?></td>
+                <td class="text-center"></td>
+                <td><?php echo $product['comment']; ?></td>
+                <td class="text-right"><a href="<?php echo zen_href_link(UN_FILENAME_WISHLIST, 'wid=' . $iID . '&products_id=' . $product['products_id'] . '&action=delete'); ?>" onclick="javascript:return confirm('Are you sure you want to Delete this record?')"><?php echo zen_image(DIR_WS_IMAGES . 'icon_delete.gif', ICON_DELETE); ?></a></td>
+              </tr>
+            <?php } ?>
 
-      <?php } else { ?>
-        <tr><td colspan="99"><?php echo TEXT_NO_RECORDS; ?></td></tr>
-
-      <?php } ?>
-
-    </table>
-    <!-- end product listing -->
-
-
+          <?php } else { ?>
+            <tr>
+              <td colspan="5"><?php echo TEXT_NO_RECORDS; ?></td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+      <!-- end product listing -->
+    </div>
     <!-- footer //-->
     <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
     <!-- footer_eof //-->
-    <br>
   </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
--- 1_Installation_Files/YOUR_ADMIN/wishlists.php
+++ 1_Installation_Files/YOUR_ADMIN/wishlists.php
@@ -1,89 +1,111 @@
 <?php
 // Includes
 require('includes/application_top.php');
-require_once(DIR_WS_CLASSES . 'wishlist_class.php');
-
-// Instantiate
-$oWishlist = new un_wishlist();
 
 // Process action
 $action = (isset($_GET['action']) ? $_GET['action'] : '');
 if (zen_not_null($action)) {
   switch ($action) {
     case 'delete':
-      $oWishlist->deleteWishlist($_GET['wid']);
+      $iID = (int)$_GET['wid'];
+      if (isset($iID) && trim($iID) != '') {
+
+        $sql = "DELETE FROM " . UN_TABLE_PRODUCTS_TO_WISHLISTS . "
+                WHERE un_wishlists_id = " . $iID;
+        $result = $db->Execute($sql);
+      }
+      if (!$result) {
+        $messageStack->add('header', 'Error deleting products from wishlist.');
+        return false;
+      }
+
+      $sql = "DELETE FROM " . UN_TABLE_WISHLISTS . "
+              WHERE id = " . $iID;
+
+      $result = $db->Execute($sql);
+      if (!$result) {
+        $messageStack->add('header', 'Error deleting wishlist.');
+        return false;
+      }
       break;
   }
 }
 
 // Get records
-$records = $oWishlist->getWishlists();
-?><!doctype html public "-//W3C//DTD HTML 4.01 Transitional//EN">
+$sql = "SELECT w.id, w.customers_id, w.created, w.modified, w.name, w.comment, w.default_status, w.public_status,
+                   COUNT(p2w.un_wishlists_id) AS items_count, c.customers_email_address, c.customers_firstname, c.customers_lastname
+            FROM " . UN_TABLE_WISHLISTS . " w
+            LEFT JOIN " . UN_TABLE_PRODUCTS_TO_WISHLISTS . " p2w ON w.id = p2w.un_wishlists_id
+            LEFT JOIN " . TABLE_PRODUCTS . " p ON p2w.products_id = p.products_id
+            LEFT JOIN " . TABLE_CUSTOMERS . " c ON w.customers_id = c.customers_id
+            GROUP BY w.id";
+$records = $db->Execute($sql);
+
+if (!$records) {
+  $messageStack->add('header', 'Error getting wishlists.');
+}
+?>
+<!doctype html>
 <html <?php echo HTML_PARAMS; ?>>
   <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=<?php echo CHARSET; ?>">
+    <meta charset="<?php echo CHARSET; ?>">
     <title><?php echo TITLE; ?></title>
-    <link rel="stylesheet" type="text/css" href="includes/stylesheet.css">
-    <link rel="stylesheet" type="text/css" href="includes/cssjsmenuhover.css" media="all" id="hoverJS">
-    <script language="javascript" src="includes/menu.js"></script>
-    <script language="javascript" src="includes/general.js"></script>
-    <script type="text/javascript">
-      <!--
-      function init()
-      {
+    <link rel="stylesheet" href="includes/stylesheet.css">
+    <link rel="stylesheet" href="includes/cssjsmenuhover.css" media="all" id="hoverJS">
+    <script src="includes/menu.js"></script>
+    <script src="includes/general.js"></script>
+    <script>
+      function init() {
           cssjsmenu('navbar');
-          if (document.getElementById)
-          {
+          if (document.getElementById) {
               var kill = document.getElementById('hoverJS');
               kill.disabled = true;
           }
       }
-      // -->
     </script>
   </head>
   <body onload="init()">
     <!-- header //-->
-<?php require(DIR_WS_INCLUDES . 'header.php'); ?>
+    <?php require(DIR_WS_INCLUDES . 'header.php'); ?>
     <!-- header_eof //-->
+    <div class="container-fluid">
+      <h1><?php echo HEADING_TITLE; ?></h1>
 
-    <h1><?php echo HEADING_TITLE; ?></h1>
-
-    <table border="0" width="100%" cellspacing="0" cellpadding="0">
-      <tr class="dataTableHeadingRow">
-        <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_CUSTOMER; ?></td>
-        <td class="dataTableHeadingContent"><?php echo TABLE_HEADING_WISHLIST; ?></td>
-        <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_COUNT; ?></td>
-        <td class="dataTableHeadingContent" align="right"><?php echo TABLE_HEADING_ACTION; ?>&nbsp;</td>
-      </tr>
-
-<?php if ($records->RecordCount() > 0) { ?>
-
-        <?php while (!$records->EOF) { ?>
-          <tr>
-            <td class="dataTableContent"><?php echo un_get_fullname($records->fields['customers_firstname'], $records->fields['customers_lastname'], $records->fields['customers_email_address']); ?></td>
-            <td class="dataTableContent"><a href="<?php echo zen_href_link(UN_FILENAME_WISHLIST, 'wid=' . $records->fields['id']); ?>"><?php echo $records->fields['name']; ?></a></td>
-            <td class="dataTableContent" align="right"><?php echo $records->fields['items_count']; ?></td>
-            <td class="dataTableContent" align="right">
-              <a href="<?php echo zen_href_link(UN_FILENAME_WISHLISTS, 'wid=' . $records->fields['id'] . '&action=delete'); ?>" onclick="javascript:return confirm('Are you sure you want to Delete this record?')"><?php echo zen_image(DIR_WS_IMAGES . 'icon_delete.gif', ICON_DELETE); ?></a>
-            </td>
+      <table class="table table-striped">
+        <thead>
+          <tr class="dataTableHeadingRow">
+            <th class="dataTableHeadingContent"><?php echo TABLE_HEADING_CUSTOMER; ?></th>
+            <th class="dataTableHeadingContent"><?php echo TABLE_HEADING_WISHLIST; ?></th>
+            <th class="dataTableHeadingContent text-right"><?php echo TABLE_HEADING_COUNT; ?></th>
+            <th class="dataTableHeadingContent text-right"><?php echo TABLE_HEADING_ACTION; ?></th>
           </tr>
-    <?php $records->MoveNext(); ?>
-        <?php } ?>
+        </thead>
+        <tbody>
+            <?php if ($records->RecordCount() > 0) { ?>
 
-      <?php } else { ?>
-        <tr>
-          <td class="dataTableContent" colspan="99"><?php echo TEXT_NO_RECORDS; ?></td>
-        </tr>
-<?php } ?>
+            <?php foreach ($records as $record) { ?>
+              <tr>
+                <td class="dataTableContent"><?php echo un_get_fullname($record['customers_firstname'], $record['customers_lastname'], $record['customers_email_address']); ?></td>
+                <td class="dataTableContent"><a href="<?php echo zen_href_link(UN_FILENAME_WISHLIST, 'wid=' . (int)$record['id']); ?>"><?php echo $record['name']; ?></a></td>
+                <td class="dataTableContent text-right"><?php echo $record['items_count']; ?></td>
+                <td class="dataTableContent text-right">
+                  <a href="<?php echo zen_href_link(UN_FILENAME_WISHLISTS, 'wid=' . (int)$record['id'] . '&action=delete'); ?>" onclick="javascript:return confirm('Are you sure you want to Delete this record?')"><?php echo zen_image(DIR_WS_IMAGES . 'icon_delete.gif', ICON_DELETE); ?></a>
+                </td>
+              </tr>
+            <?php } ?>
 
-
-    </table>
-
+          <?php } else { ?>
+            <tr>
+              <td class="dataTableContent text-center" colspan="4"><?php echo TEXT_NO_RECORDS; ?></td>
+            </tr>
+          <?php } ?>
+        </tbody>
+      </table>
+    </div>
 
     <!-- footer //-->
-<?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
+    <?php require(DIR_WS_INCLUDES . 'footer.php'); ?>
     <!-- footer_eof //-->
-    <br>
   </body>
 </html>
 <?php require(DIR_WS_INCLUDES . 'application_bottom.php'); ?>
